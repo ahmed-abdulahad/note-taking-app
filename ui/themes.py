d42d4cc0baf71c39737c52745d4f90be2c3c6915
Ahmed Abdulahad <ahmed.abdulahad@protonmail.com>
2025-06-05 12:34:24 +0200

    Initial commit: Set up dark and light theme

---
+++ ui/themes.py
@@ -0,0 +1,53 @@
+dark_theme = """
+    QMainWindow{
+        background-color: #1a1a40;
+    }
+    QPushButton{
+        background-color: #141430;
+        color: #ffffff;
+        font-family: Arial;
+        font-size: 30px;
+        padding: 10px;
+        border-radius: 25px;
+        min-width: 100px;
+        min-height: 50px;
+    }
+    QPushButton:hover{
+        background-color: #2a2a60;
+    }
+    QListWidget, QTextEdit{
+        background-color: #2a2a60;
+        color: #ffffff;
+        font-family: Arial;
+        font-size: 30px;
+        padding: 10px 10px;
+        border: None;
+    }
+"""
+
+light_theme = """
+    QMainWindow{
+        background-color: #e8e8e8;
+    }
+    QPushButton{
+        background-color: #ffffff;
+        color: #000000;
+        font-family: Arial;
+        font-size: 30px;
+        padding: 10px;
+        border-radius: 25px;
+        min-width: 100px;
+        min-height: 50px;
+    }
+    QPushButton:hover{
+        background-color: #d4d4d4;
+    }
+    QListWidget, QTextEdit{
+        background-color: #ffffff;
+        color: #000000;
+        font-family: Arial;
+        font-size: 30px;
+        padding: 10px 10px;
+        border: None;
+    }
+"""
